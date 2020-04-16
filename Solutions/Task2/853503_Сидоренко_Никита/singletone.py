10e89f2b093b05a4ffd2b33be90f8c1b729d18cf
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-04-16 20:01:47 +0300

    Update singletone.py

--- Solutions/Task2/853503_Сидоренко_Никита/singletone.py
+++ Solutions/Task2/853503_Сидоренко_Никита/singletone.py
@@ -1,8 +1,16 @@
-class Singleton(object):
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Singleton, cls).__new__(cls)
-        return cls.instance
+class Singleton(type):
+    exemplar = {}
 
-class HeirSingleton(Singleton):
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.exemplar:
+            cls.exemplar[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls.exemplar[cls]
+
+
+class SimpleSingleton(metaclass=Singleton):
     pass
+
+
+class SingletonWithValue(metaclass=Singleton):
+    def __init__(self, value):
+        self.value = value
