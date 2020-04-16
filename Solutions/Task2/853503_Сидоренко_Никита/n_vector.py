5ba4f33e6b1b852a7ae4693c29170f800d9de581
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-04-16 19:32:04 +0300

    Update n_vector.py

--- Solutions/Task2/853503_Сидоренко_Никита/n_vector.py
+++ Solutions/Task2/853503_Сидоренко_Никита/n_vector.py
@@ -5,12 +5,13 @@ class N_Vector:
     def __init__(self, string):
         self._dimension = []
         self._size = 0
-        if(type(string) == str):
+        try:
             for item in string.split(' '):
                 self._dimension.append(int(item))
                 self._size = self._size + 1
-        else:
-            print('Неверный формат')
+        except ValueError:
+            self._dimension = []
+
 
     def multiplication_by_constant(self, const):
         self._dimension = list(map(lambda x : x * const, self._dimension))
@@ -25,25 +26,26 @@ class N_Vector:
     def get_item(self, index):
         return self._dimension[index]
 
-    def sum(self, vector2):
+    def __add__(self, vector2):
         return list(map(lambda x, y : x + y, self._dimension, vector2._dimension))
 
-    def subtraction(self, vector2):
+    def __sub__(self, vector2):
         return list(map(lambda x, y : x - y, self._dimension, vector2._dimension))
 
-    def multiplication(self, vector2):
+    def __mul__(self, vector2):
         return list(map(lambda x, y: x * y, self._dimension, vector2._dimension))
 
-    def division(self, vector2):
+    def __truediv__(self, vector2):
         return list(map(lambda x, y: x / y, self._dimension, vector2._dimension))
+
+    def __str__(self):
+        return '{}'.format(self._dimension)
+
+    def __eq__(self, vector2):
+        return True if self._dimension == vector2._dimension else False
 
     def scalar_product(self, vector2):
         sum = 0
         for item in list(map(lambda x, y: x * y, self._dimension, vector2._dimension)):
            sum += item
         return sum
-
-    def equals(self, vector2):
-        if self._dimension == vector2._dimension:
-            return True
-        return False
